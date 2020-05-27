2748d7517adc8c66da155593c332d2b416f37c47
drzenit <starikovandru@gmail.com>
2020-05-28 01:01:15 +0300

    done textureAnalysis

--- textureAnalysis.py
+++ textureAnalysis.py
@@ -1,35 +1,95 @@
-import matplotlib.pyplot as plt
-
-from skimage.feature import greycomatrix, greycoprops
-from skimage import data, io
-from PIL import Image, ImageDraw
-import numpy as np
+from matplotlib import pyplot as plt
 import pandas as pd
+from skimage.feature import greycomatrix, greycoprops
+from skimage import io
 
 
-# open the camera image
-#img = Image.open("data\Powdery_mildew\\1.jpg")
+# Чтение картинки в память
+def readImage(dataPath: str):
+    # Чтение изображения
+    image = io.imread(dataPath)
 
-#grayscale = img.convert('L')
-#image = np.asarray(grayscale)
+    # Вывод прочитанного изображения
+    io.imshow(image)
+    plt.title("Исходное изображение")
+    plt.show()
 
-image = io.imread("data\Powdery_mildew\\1.jpg", as_gray=True)
-image = np.round(image * 255)
-image = image.astype(int)
+    return image
+
+# Разбиение изображения на RGB слои
+def rgbSplit(image: io.imread):
+    r = image[:, :, 0]
+    g = image[:, :, 1]
+    b = image[:, :, 2]
+
+    # Вывод слоев изображения
+    io.imshow(r)
+    plt.title("Красный R")
+    plt.show()
+
+    io.imshow(g)
+    plt.title("Зеленый G")
+    plt.show()
+
+    io.imshow(b)
+    plt.title("Синий B")
+    plt.show()
+
+    return r, g, b
+
+# Получение текстурных параметров изображения на основе glcm (статистические характеристики)
+def getTextureParam(r: io.imread, g: io.imread, b: io.imread):
+    # Выделение смешанных слоев rgb
+    rg = r - g
+    rb = r - b
+    gb = g - b
+
+    # Вывод смешанных слоев изображения
+    io.imshow(rg)
+    plt.title("Красный-Зеленый RG")
+    plt.show()
+
+    io.imshow(rb)
+    plt.title("Красно-Синий RB")
+    plt.show()
+
+    io.imshow(gb)
+    plt.title("Зелено-Синий GB")
+    plt.show()
+
+    # Получение glcm матриц и статистических характеристик для слоев RGB
+    glcm = greycomatrix(r, distances=[2], angles=[0], levels=256)
+    statsR = greycoprops(glcm)
+    glcm = greycomatrix(g, distances=[2], angles=[0], levels=256)
+    statsG = greycoprops(glcm)
+    glcm = greycomatrix(b, distances=[2], angles=[0], levels=256)
+    statsB = greycoprops(glcm)
+
+    # Получение glcm матриц и статистических характеристик для смешанных слоев RGB
+    glcm = greycomatrix(rg, distances=[2], angles=[0], levels=256)
+    statsRG = greycoprops(glcm)
+    glcm = greycomatrix(rb, distances=[2], angles=[0], levels=256)
+    statsRB = greycoprops(glcm)
+    glcm = greycomatrix(gb, distances=[2], angles=[0], levels=256)
+    statsGB = greycoprops(glcm)
+
+    # Перевод данных в pd.DataFrame для удобства
+    statsR_DF = pd.DataFrame(statsR)
+    statsG_DF = pd.DataFrame(statsG)
+    statsB_DF = pd.DataFrame(statsB)
+    statsRG_DF = pd.DataFrame(statsRG)
+    statsRB_DF = pd.DataFrame(statsRB)
+    statsGB_DF = pd.DataFrame(statsGB)
+
+    # Вывод статистических текстурных характеристик
+    print("Текстурные данные R ", statsR_DF)
+    print("Текстурные данные G ", statsG_DF)
+    print("Текстурные данные B ", statsB_DF)
+    print("Текстурные данные RG ", statsRG_DF)
+    print("Текстурные данные RB ", statsRB_DF)
+    print("Текстурные данные GB ", statsGB_DF)
 
 
-
-
-glcm = greycomatrix(image, distances=[2], angles=[0], levels=256)
-gr = greycoprops(glcm, "homogeneity")
-
-print(gr)
-
-# compute some GLCM properties each patch
-xs = []
-ys = []
-
-
-print(xs)
-print(ys)
-# create the figure
+image = readImage("data\Brown_rust\\1.jpg")
+r, g, b = rgbSplit(image)
+getTextureParam(r, g, b)
